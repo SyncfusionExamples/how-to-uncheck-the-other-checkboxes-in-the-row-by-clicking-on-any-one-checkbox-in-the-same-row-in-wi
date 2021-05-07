9fa712cc57932619d83aaf02ac85fbf46e553596
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-05-07 11:06:54 +0530

    Sample Added
    Sample Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SfDataGridDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfDataGrid1 = new Syncfusion.WinForms.DataGrid.SfDataGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfDataGrid1
+            // 
+            this.sfDataGrid1.AccessibleName = "Table";
+            this.sfDataGrid1.AllowFiltering = true;
+            this.sfDataGrid1.AllowResizingColumns = true;
+            this.sfDataGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sfDataGrid1.AutoSizeColumnsMode = Syncfusion.WinForms.DataGrid.Enums.AutoSizeColumnsMode.Fill;
+            this.sfDataGrid1.Location = new System.Drawing.Point(10, 5);
+            this.sfDataGrid1.Margin = new System.Windows.Forms.Padding(5);
+            this.sfDataGrid1.Name = "sfDataGrid1";
+            this.sfDataGrid1.RowHeight = 21;
+            this.sfDataGrid1.ShowGroupDropArea = true;
+            this.sfDataGrid1.Size = new System.Drawing.Size(787, 397);
+            this.sfDataGrid1.TabIndex = 0;
+            this.sfDataGrid1.Text = "sfDataGrid1";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(811, 407);
+            this.Controls.Add(this.sfDataGrid1);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Binding";
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Syncfusion.WinForms.DataGrid.SfDataGrid sfDataGrid1;
+        #endregion
+    }
+}
+
